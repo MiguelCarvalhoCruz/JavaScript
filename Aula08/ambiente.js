f6227624b633a16650d0f3bbd42a4c9bc41ba1d2
MiguelCarvalhoCruz <miguelcarvalhocruz1001@gmail.com>
2025-05-10 21:09:37 -0300

    Atualizado

---
+++ Aula08/ambiente.js
@@ -0,0 +1,21 @@
+var contador = 1
+
+do {
+
+    console.log(`Valor de ${contador}`)
+    contador++ // contador está rececendo o valor do contador 1 + 1.
+} while (contador <= 10)
+
+
+
+/* Teste Logico no começo
+
+var contador = 1
+
+while (contador < 6) {
+
+    console.log(`Valor de ${contador}`)
+    contador++ // contador está rececendo o valor do contador 1 + 1.
+}
+
+*/
